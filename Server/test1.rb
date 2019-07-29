f7e2f89fbf4e779b596eaab6317ddaf53ba9e5d4
nvolnikov <nvolnikov@rencredit.ru>
2019-07-29 18:25:14 +0300

    Добавлены изменения условии в запросе

--- Server/test1.rb
+++ Server/test1.rb
@@ -1,15 +1,24 @@
 require "sinatra"
 require 'json'
 
+ # Возвращает массив описаний тем
+post '/getTopics' do
+
+    end
+
+
+
 post '/post' do
   mas = params
 
   if mas["login"] == "Volnikov"
     content_type :json
     {"Volnikov" => "True"}.to_json
+  elsif mas["login"] == "Platon"
+    {"Platon" => "True"}.to_json
   else
     {"Fail"=>"Fail"}.to_json
-
   end
+
 
 end
